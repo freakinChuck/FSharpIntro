816c9cd89e01c34b0919ec3db0d142f187ee3489
Samuel Bittmann <samuel.bittmann@stud.hslu.ch>
2016-12-08 22:01:12 +0100

    Improved selection menu

--- FSharpIntro/Program.fs
+++ FSharpIntro/Program.fs
@@ -1,19 +1,37 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+type MenuItem = { name:string; action:(unit->unit) }
+
+let menuItems = [
+        { name="Agentenprogrammierung"; action=Agentenprogrammierung.demo }
+        { name="Parameteruebergabe"; action=Parameteruebergabe.demo }
+    ]
+
+let (|Int|_|) string = 
+    match System.Int32.TryParse(string) with
+        | (true, int) -> Some int
+        | _ -> None
+
+let (|InRange|_|) low high integer = 
+    match integer with
+        | i when i >= low && i <= high -> Some integer
+        | _ -> None
+
 let rec selectAction () =
     System.Console.Clear ()
     printf "**********************\n"
     printf "* F# Demos           *\n"
     printf "**********************\n"
-    printf "1) Agentenprogrammierung\n"
-    printf "2) Parameteruebergabe\n"
+    menuItems |> List.fold (fun i item ->
+        printf "%d) %s\n" i item.name
+        i + 1
+        ) 1 |> ignore
     printf "q) Abbrechen\n"
     printf "Auswahl: "
     let input = System.Console.ReadLine ()
     match input with
-        | "1" -> Option.Some Agentenprogrammierung.demo
-        | "2" -> Option.Some Parameteruebergabe.demo
+        | Int i when i > 0 && i <= menuItems.Length -> Option.Some (menuItems.Item (i - 1)).action
         | "q" -> Option.None
         | _ -> selectAction ()
 
