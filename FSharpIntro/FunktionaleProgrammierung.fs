00b4d11be34434a9ec4c31b89033729263c828ce
Silvio Stappung <silvio.stappung@stud.hslu.ch>
2016-12-16 13:44:07 +0100

    new Exaple for functional Programming

--- FSharpIntro/FunktionaleProgrammierung.fs
+++ FSharpIntro/FunktionaleProgrammierung.fs
@@ -5,13 +5,26 @@ let mul x y = x*y
 //Methode DoMath, welche zwei Zahlen und einen Operator entgegennimmt
 let doMath x y op = op y x
 
-let demo () =     
+ 
+let demoFirstClassCitizen () =     
     printfn "----------------------------------------------------------------"
-    printfn "FunktionaleProgrammierung.demo"
+    printfn "FunktionaleProgrammierung.demoFCC"
     printfn "let mul x y = x*y"
     printfn "let doMath x y op = op y x"
     printfn "let result = doMath 5 6 mul"
     // rufe die Methide DoMath für die Berechnung von (5 * 6) auf
     let result = doMath 5 6 mul
     printfn "result = %A" result
+
+let demo () = 
+    printfn "FunktionaleProgrammierung.demo"
+    let sum = 
+        [1..4] 
+        |> Seq.filter(fun x -> x < 4) 
+        |> Seq.sum
+
+    printfn "let sum = [1..4] |> Seq.filter(fun x -> x < 4) |> Seq.sum"
+
+    printfn "sum = %A" sum
+    demoFirstClassCitizen ()
     
